f014279e7b66d202ef7ca31aacd1e54286214631
pallab-gain <pallab.gain@gmail.com>
2014-06-14 11:10:00 +0600

    initial commit

---
+++ ean13/ean13.py
@@ -0,0 +1,71 @@
+__author__ = 'xerxes'
+
+
+class EAN13:
+    product = manufacture = country = None
+    code = checksum = None
+
+    def __init__(self, country_code=88, manufacturer_code=12345, product_code=12345):
+        self.country = self.sanitize_ean13(country_code, 'C')
+        self.manufacture = self.sanitize_ean13(manufacturer_code, 'M')
+        self.product = self.sanitize_ean13(product_code, 'P')
+
+
+    @staticmethod
+    def sanitize_ean13(number=None, s_type='C'):
+        if not number:
+            raise Exception('Number cannot be empty')
+
+        cur_len = None
+        if s_type == 'C':
+            if not isinstance(number, int):
+                raise Exception('Country code must be integer')
+            if str(number).__len__() > 2:
+                raise Exception('country code length must not be less than two')
+            cur_len = 2
+        elif s_type == 'M':
+            if not isinstance(number, int):
+                raise Exception('manufacturer code must be integer')
+            if str(number).__len__() > 5:
+                raise Exception('manufacturer code length must not be less than five')
+            cur_len = 5
+        else:
+            if not isinstance(number, int):
+                raise Exception('product code must be integer')
+            if str(number).__len__() > 5:
+                raise Exception('product code length must not be less than five')
+            cur_len = 5
+
+        number = str(number)
+        while number.__len__() < cur_len:
+            number = '0' + number
+        return number
+
+    def set_country(self, country_code=88):
+        self.country = self.sanitize_ean13(country_code, 'C')
+
+    def set_manufacturer(self, manufacturer_code=12345):
+        self.manufacture = self.sanitize_ean13(manufacturer_code, 'M')
+
+    def set_product(self, product_code=12345):
+        self.product = self.sanitize_ean13(product_code, 'P')
+
+    def merge(self):
+        self.code = ''.join([self.country, self.manufacture, self.product])
+
+    def calculate_checksum(self):
+        odd_sum = 0
+        evn_sum = 0
+        for i in xrange(0, 12):
+            if i and 1:
+                odd_sum += int(self.code[i])
+            else:
+                evn_sum += int(self.code[i])
+
+        self.checksum = str((10 - (((3 * odd_sum) + evn_sum) % 10)) % 10)
+
+    def gen_ean(self, product_code=12345):
+        self.set_product(product_code)
+        self.merge()
+        self.calculate_checksum()
+        return ''.join([self.code, self.checksum])
---
+++ readme.md
@@ -0,0 +1,64 @@
+Yet Another EAN13 code generator
+    How to use:
+        class name :
+            EAN13
+        parameters :
+            country_code ( default = 88 ) : type integer
+            manufacturer_code ( default = 12345 ): type integer
+            product_code ( default = 12345 ) : type integer
+
+        return:
+            thirteen digit ean13 number: type string
+
+        available methods:
+            1. sanitize_ean13:
+                    parameters:
+                        number ( mandatory ) : type integer
+                        s_type ( default = 'C' ) : type char
+                            available s_type:
+                                'C' for country
+                                'M' for manufacturer
+                                anything else for product
+                    return:
+                        sanitized country, manufacturer, or product code : type string
+
+
+
+            2. set_country:
+                    parameters:
+                        //takes two digit country code
+                        country_code ( default = 88 ): type integer
+                    return:
+                        none
+            3. set_manufacturer:
+                    parameters:
+                        //takes five digit manufacturer code
+                        manufacturer_code( default = 12345): type integer
+                    return:
+                        none
+            4. set_product:
+                    parameters:
+                        //takes five digit product code
+                        product_code( default = 12345): type integer
+                    return:
+                        none
+
+            5. merge:
+                    parameters:
+                        //merge country, manufacture and product code in order to get 12 digit ean number
+                        none
+                    return:
+                        none
+
+            6. calculate_checksum
+                    parameters:
+                        //calculate checksum of 12 digit ean number
+                        none
+                    return:
+                        none
+            7. gen_ean:
+                    parameters:
+                        //generate 13 digit ean13 number
+                        product_code ( default = 12345 ): type integer
+                    return:
+                        13 digit ean13 number: type string
